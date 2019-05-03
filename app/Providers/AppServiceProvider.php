d768d68f62a3316560a2e6ed8624033a8a38e13a
Atiladanvi <atila.danvi@outlook.com>
2019-05-03 13:23:46 -0300

    Install Reliese

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Reliese\Coders\CodersServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->environment() == 'local') {
+            $this->app->register(CodersServiceProvider::class);
+        }
     }
 
     /**
