46bac309e1a671e40c7439f3ba47d6566027ecf0
Atiladanvi <atila.danvi@outlook.com>
2019-05-06 12:01:59 -0300

    Install spatie permissions

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Reliese\Coders\CodersServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
 
     ],
 
