e68e2d0c380b8247f9592d8f6598a86eb92e00af
Atiladanvi <atila.danvi@outlook.com>
2019-05-07 15:56:09 -0300

    Adjust factories

--- database/factories/ModelsFactory.php
+++ database/factories/ModelsFactory.php
@@ -3,10 +3,13 @@
 
 use Illuminate\Support\Str;
 
+$faker = app('Faker\Generator');
+$name = $faker->name;
+
 $factory->define(App\Models\Tag::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'description' => $faker->word,
+        'name' => $faker->word,
+        'description' => $faker->text,
     ];
 });
 
@@ -18,18 +21,18 @@ $factory->define(App\Models\Favorite::class, function (Faker\Generator $faker) {
     ];
 });
 
-$factory->define(App\Models\Blog::class, function (Faker\Generator $faker) {
+$factory->define(App\Models\Blog::class, function (Faker\Generator $faker) use ($name){
     return [
-        'name' => $faker->name,
+        'name' => $name,
         'description' => $faker->text,
         'sub_domain' => $faker->word,
         'guard_name' => $faker->word,
     ];
 });
 
-$factory->define(App\Models\User::class, function (Faker\Generator $faker) {
+$factory->define(App\Models\User::class, function (Faker\Generator $faker) use ($name) {
     return [
-        'name' => $faker->name,
+        'name' => $name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => bcrypt('senha'),
@@ -38,26 +41,16 @@ $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
 });
 
 $factory->afterCreating(\App\Models\User::class, function (\App\Models\User $user) {
-   $userHasBlog = factory(\App\Models\UserHasBlog::class)->create([
-       'blog_id' => factory(\App\Models\Blog::class)->create()->id,
-       'user_id' => $user->id
-   ]);
-
-   for ($n = 1 ; $n <= rand(1,10); $n++){
-       factory(\App\Models\Post::class)->create([
-           'user_has_blog_id' => $userHasBlog->id
-       ]);
-   }
-});
-
-$factory->define(\App\Models\UserHasBlog::class, function (){
-   return [];
+    factory(\App\Models\Post::class, rand(20,100))->create([
+        'blog_id' => factory(\App\Models\Blog::class)->create(['user_id' => $user->id])->id,
+        'user_id' => $user->id
+    ]);
 });
 
 
 $factory->define(App\Models\Post::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
+        'name' => $faker->text(25),
         'content' => $faker->text
     ];
 });
