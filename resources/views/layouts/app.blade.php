3d40692b4aaa2540658f2e1b3edf4f59459d0bfa
Atiladanvi <atila.danvi@outlook.com>
2019-05-07 15:57:01 -0300

    Resource project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.png') }}">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'HasBlog') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
