d7d3fcfb3b69a41a58b3fd3319c1d99cdf7276b3
Atiladanvi <atila.danvi@outlook.com>
2019-07-25 22:36:03 -0300

    Force https in production mode.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
+        if(config('app.env') === 'local') {
+            \URL::forceScheme('http');
+        }
     }
 }
