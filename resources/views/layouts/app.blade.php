bc9b5b446eedfd04c7a02cfca4b66550df9f1e59
Atiladanvi <atila.danvi@outlook.com>
2019-08-31 21:17:44 -0300

    Add custom typography to tiny editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -131,10 +131,6 @@
                 'insertdatetime media table paste code help wordcount'
             ],
             toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
-            content_css: [
-                '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
-                '//www.tiny.cloud/css/codepen.min.css'
-            ]
         });
     });
 </script>
