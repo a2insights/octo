747385548443bfa8b1b11cb83b17faa1b2e56743
Atiladanvi <atila.danvi@outlook.com>
2020-07-05 00:11:17 -0300

    Add tenancy package

---
+++ database/migrations/2019_09_15_000010_create_tenants_table.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTenantsTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('tenants', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->timestamps();
+            $table->json('data')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('tenants');
+    }
+}
