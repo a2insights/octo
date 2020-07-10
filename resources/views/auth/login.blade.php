47e626eaae6de4e41c7976cef454b22ba4360874
Atiladanvi <atila.danvi@outlook.com>
2020-07-10 17:30:48 -0300

    Improve a new front-end

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,16 +1,19 @@
 @extends('layouts.site')
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-lg-4">
-                <h2 class="text-center my-3">{{ __('Login') }}</h2>
-                <div class="card mt-4">
-                    <div class="card-body px-4 pb-2 pt-4">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
-                            <div class="form-group">
-                                <label for="email">{{ __('E-Mail') }}</label>
-                                <input
+    <div class="masthead text-white">
+        <div class="masthead-content">
+            <h2 class="text-center">Sign In!</h2>
+            <div class="row">
+                <div class="col align-self-start">
+                </div>
+                <div class="col-md-3 align-self-center">
+                    <div class="card bg-dark mt-4 mx-2">
+                        <div class="card-body px-4 pb-2 pt-4">
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="email">{{ __('E-Mail') }}</label>
+                                    <input
                                         id="email"
                                         type="email"
                                         class="form-control @error('email') is-invalid @enderror"
@@ -18,56 +21,57 @@
                                         required
                                         autocomplete="email"
                                         autofocus
-                                >
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
+                                    >
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
-                                @enderror
-                            </div>
-                            <div class="form-group">
-                                <label for="password">{{ __('Password') }}</label>
-                                <input
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">{{ __('Password') }}</label>
+                                    <input
                                         id="password"
                                         type="password"
                                         class="form-control @error('password') is-invalid @enderror"
                                         name="password"
                                         required
                                         autocomplete="current-password"
-                                >
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
+                                    >
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
-                                @enderror
-
-                                <div class="form-group mt-3">
-                                    <div class="form-check">
-                                        <input
+                                    @enderror
+                                    <div class="form-group mt-3">
+                                        <div class="form-check">
+                                            <input
                                                 class="form-check-input"
                                                 type="checkbox"
                                                 name="remember"
                                                 id="remember" {{ old('remember') ? 'checked' : '' }}
-                                        >
-                                        <label class="form-check-label " for="remember">
-                                            {{ __('Remember Me') }}
-                                        </label>
+                                            >
+                                            <label class="form-check-label " for="remember">
+                                                {{ __('Remember Me') }}
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group mb-0">
+                                        <button type="submit" class="btn btn-block btn-md btn-outline-primary">
+                                            {{ __('Login') }}
+                                        </button>
+                                        @if (Route::has('password.request'))
+                                            <a class="btn mt-3 btn-link pl-0" href="{{ route('password.request') }}">
+                                                {{ __('Forgot Your Password?') }}
+                                            </a>
+                                        @endif
                                     </div>
                                 </div>
-
-                                <div class="form-group mb-0">
-                                    <button type="submit" class="btn btn-block btn-md btn-outline-primary">
-                                        {{ __('Login') }}
-                                    </button>
-                                    @if (Route::has('password.request'))
-                                        <a class="btn mt-3 btn-link pl-0" href="{{ route('password.request') }}">
-                                            {{ __('Forgot Your Password?') }}
-                                        </a>
-                                    @endif
-                                </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
+                </div>
+                <div class="col align-self-end">
                 </div>
             </div>
         </div>
--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -1,62 +1,54 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="h-100" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.png') }}">
-    <title>Has Blog</title>
-    <!-- Scripts -->
+    <title>{{ config('app.name', 'Blog') }}</title>
     <script src="{{ asset('js/app.js') }}" defer></script>
-
-    @toastr_css
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @notify_css
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
 </head>
-<body class="bg-white">
-<header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-
-                </ul>
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    @if(Auth::user())
+<body class="d-flex flex-column h-100">
+<nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'Laravel') }}
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                @if(Auth::user())
+                    <li class="nav-item">
+                        <a class="nav-link" href="/{{ Auth::user()->blog()->path }}">{{ __('Blog') }}</a>
+                    </li>
+                @endif
+                @auth
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('dashboard', Auth::user()->blog()->tenant_id) }}">{{ __('Dashboard') }}</a>
+                    </li>
+                @endauth
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('SIGN IN') }}</a>
+                    </li>
+                    @if (Route::has('register'))
                         <li class="nav-item">
-                            <a class="nav-link" href="/{{ Auth::user()->blog()->path }}">{{ __('Blog') }}</a>
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('New blog') }}</a>
                         </li>
                     @endif
-                    @auth
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('dashboard', Auth::user()->blog()->tenant_id) }}">{{ __('Dashboard') }}</a>
-                        </li>
-                    @endauth
-                    @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
+                @else
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
-
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
@@ -68,35 +60,160 @@
                             </form>
                         </div>
                     </li>
-                    @endguest
-                </ul>
-            </div>
+                @endguest
+            </ul>
         </div>
-    </nav>
-</header>
-
-<main id="app">
+    </div>
+</nav>
+<main role="main" class="flex-shrink-0" id="app">
     @yield('content')
 </main>
-
-<footer class="footer border bg-light">
-    <div class="container text-center">
-        <span class="text-muted">@hasBlog - 2019</span>
+<footer class="bg-dark footer mt-auto py-3">
+    <div class="container">
+        <p class="m-0 text-center text-white small">Copyright &copy; HasBlog 2020</p>
     </div>
 </footer>
-
 </body>
-@jquery
-@toastr_js
-@toastr_render
+@notify_js
+@notify_render
 <style>
-    .footer {
-        position: fixed;
-        width: 100%;
-        bottom: 0;
-        height: 60px; /* Set the fixed height of the footer here */
-        line-height: 60px; /* Vertically center the text there */
+    body {
+        font-family: 'Lato', serif;
+        background: linear-gradient(0deg, #ff6a00 0%, #ee0979 100%);
     }
-</style>
 
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+        font-family: 'Catamaran', serif;
+        font-weight: 800 !important;
+    }
+
+    .btn-xl {
+        text-transform: uppercase;
+        padding: 1.5rem 3rem;
+        font-size: 0.9rem;
+        font-weight: 700;
+        letter-spacing: 0.1rem;
+    }
+
+    .rounded-pill {
+        border-radius: 5rem;
+    }
+
+    .navbar-custom {
+        padding-top: 1rem;
+        padding-bottom: 1rem;
+        background-color: rgb(239, 18, 108);
+    }
+
+    .navbar-custom .navbar-brand {
+        text-transform: uppercase;
+        font-size: 1rem;
+        letter-spacing: 0.1rem;
+        font-weight: 700;
+    }
+
+    .navbar-custom .navbar-nav .nav-item .nav-link {
+        text-transform: uppercase;
+        font-size: 0.8rem;
+        font-weight: 700;
+        letter-spacing: 0.1rem;
+    }
+
+    .masthead {
+        position: relative;
+        overflow: hidden;
+        padding-top: calc(7rem + 72px);
+        padding-bottom: 7rem;
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-attachment: scroll;
+        background-size: cover;
+    }
+
+    .masthead .masthead-content {
+        z-index: 1;
+        position: relative;
+    }
+
+    .masthead .masthead-content .masthead-heading {
+        font-size: 10rem;
+    }
+
+    .masthead .masthead-content .masthead-subheading {
+        font-size: 2rem;
+    }
+
+    .bg-circle {
+        z-index: 0;
+        position: absolute;
+        border-radius: 100%;
+        background: linear-gradient(0deg, #ee0979 0%, #ff6a00 100%);
+    }
+
+    .bg-circle-1 {
+        height: 90rem;
+        width: 90rem;
+        bottom: -55rem;
+        left: -55rem;
+    }
+
+    .bg-circle-2 {
+        height: 50rem;
+        width: 50rem;
+        top: -25rem;
+        right: -25rem;
+    }
+
+    .bg-circle-3 {
+        height: 20rem;
+        width: 20rem;
+        bottom: -10rem;
+        right: 5%;
+    }
+
+    .bg-circle-4 {
+        height: 30rem;
+        width: 30rem;
+        top: -5rem;
+        right: 35%;
+    }
+
+    .bg-primary {
+        background-color: #ee0979 !important;
+    }
+
+    .btn-primary {
+        background-color: #ee0979;
+        border-color: #ee0979;
+    }
+
+    .btn-primary:active, .btn-primary:focus, .btn-primary:hover {
+        background-color: #bd0760 !important;
+        border-color: #bd0760 !important;
+    }
+
+    .btn-primary:focus {
+        box-shadow: 0 0 0 0.2rem rgba(238, 9, 121, 0.5);
+    }
+
+    .btn-secondary {
+        background-color: #ff6a00;
+        border-color: #ff6a00;
+    }
+
+    .btn-secondary:active, .btn-secondary:focus, .btn-secondary:hover {
+        background-color: #cc5500 !important;
+        border-color: #cc5500 !important;
+    }
+
+    .btn-secondary:focus {
+        box-shadow: 0 0 0 0.2rem rgba(255, 106, 0, 0.5);
+    }
+
+</style>
 </html>
