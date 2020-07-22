7f7a2754b9317ad907d2ae421c5cfa152ab503c6
Atiladanvi <atila.danvi@outlook.com>
2020-07-21 22:36:58 -0300

    Change logo concept

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -16,7 +16,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
     <div class="container">
         <a class="navbar-brand"  href="{{ url('/') }}">
-            <img width="80px" src="{{asset('img/brand-white.svg')}}" alt="logo tenancy">
+            OCTO
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
