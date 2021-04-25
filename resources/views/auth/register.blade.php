9da81aefeb3b88416572c81b7101369951bc90e5
Atiladanvi <atila.danvi@outlook.com>
2021-04-25 01:58:52 -0300

    Adjust logo and positions for auth views

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -46,15 +46,15 @@
                 </div>
             @endif
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-between mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
                     {{ __('Already registered?') }}
                 </a>
-
                 <x-jet-button class="ml-4">
                     {{ __('Register') }}
                 </x-jet-button>
             </div>
+
         </form>
 
         @if (JoelButcher\Socialstream\Socialstream::show())
