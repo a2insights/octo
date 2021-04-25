2ba90e586020ed8e2b9a6c99aba89b3960dffa61
Atiladanvi <atila.danvi@outlook.com>
2021-04-25 01:43:06 -0300

    Replace jet logo for octo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <x-octo-authentication-card-logo />
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <x-octo-authentication-card-logo />
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
