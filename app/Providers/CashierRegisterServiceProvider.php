21506cbf057eae50a679295f46cd75020f87d921
Atiladanvi <atila.danvi@outlook.com>
2021-10-22 20:54:26 -0300

    Add defaults plans

--- app/Providers/CashierRegisterServiceProvider.php
+++ app/Providers/CashierRegisterServiceProvider.php
@@ -18,23 +18,23 @@ class CashierRegisterServiceProvider extends BaseServiceProvider
 
         Saas::currency('BRL');
 
-        $free = Saas::plan('Free Plan', 'price_1JgbF1KBVLqcMf8uOBf7NYAF')
+        Saas::plan('Free Plan', 'price_1JgbF1KBVLqcMf8uOBf7NYAF')
             ->monthly(0)
             ->features([
                 Saas::feature('5 Seats', 'seats', 5)->notResettable(),
                 Saas::feature('10,000 mails', 'mails', 10000),
             ]);
 
-        Saas::plan('Tier One', 'price_1Jh22oKBVLqcMf8ufFUi7upc')
-            ->monthly(10)
+        Saas::plan('Tier One', 'price_1IriI3KBVLqcMf8ufdEbBfEp')
+            ->monthly(5)
             ->features([
                 Saas::feature('Unlimited Seats', 'seats')->unlimited()->notResettable(),
                 Saas::feature('20,000 mails', 'mails', 20000),
                 Saas::feature('Beta Access', 'beta.access')->unlimited(),
             ]);
 
-        Saas::plan('Tier Two', 'price_2')
-            ->monthly(20)
+        Saas::plan('Tier Two', 'price_1Jh22oKBVLqcMf8ufFUi7upc')
+            ->monthly(10)
             ->features([
                 Saas::feature('Unlimited Seats', 'seats')->unlimited()->notResettable(),
                 Saas::feature('30,000 mails', 'mails', 30000),
