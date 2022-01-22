f8e6e73510b6828d7555413fa877251106960983
Atiladanvi <atila.danvi@outlook.com>
2022-01-22 12:18:01 -0300

    Add docker sail and fixes from upgrades

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,42 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./vendor/laravel/jetstream/**/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
-
+    safelist: [
+        "sm:max-w-sm",
+        "sm:max-w-md",
+        "sm:max-w-lg",
+        "sm:max-w-xl",
+        "sm:max-w-2xl",
+        "sm:max-w-3xl",
+        "sm:max-w-4xl",
+        "sm:max-w-5xl",
+        "sm:max-w-6xl",
+        "sm:max-w-7xl",
+        "md:max-w-lg",
+        "md:max-w-xl",
+        "lg:max-w-2xl",
+        "lg:max-w-3xl",
+        "xl:max-w-4xl",
+        "xl:max-w-5xl",
+        "2xl:max-w-6xl",
+        "2xl:max-w-7xl'",
+    ],
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ["Nunito", ...defaultTheme.fontFamily.sans],
             },
         },
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+    ],
 };
