9f68cebef2f318c131329fad8f0d19f55d1715ec
a21ns1g4ts <atila.danvi@outlook.com>
2022-03-01 17:13:50 -0300

    Wip and update

--- server_deploy.sh
+++ server_deploy.sh
@@ -19,7 +19,22 @@ echo "Deploying application ..."
     npm run production
 
     # Setup new aplication
-    php artisan octo:setup
+    php artisan octo:demo
+
+    # Clear cache
+    php artisan cache:clear
+
+    # Clear config cache
+    php artisan config:clear
+
+    # Clear route cache
+    php artisan route:clear
+
+    # Clear view cache
+    php artisan view:clear
+
+    # Clear compiled class cache
+    php artisan clear-compiled
 
     # Reload PHP to update opcache
     echo "" | sudo -S service php8.0-fpm reload
