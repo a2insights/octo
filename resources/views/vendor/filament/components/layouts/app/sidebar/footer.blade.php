fe19cc8798864b46cfa64a1e2303b7e6e1b24c2d
a21ns1g4ts <atila.danvi@outlook.com>
2022-03-29 09:49:15 -0300

    Updates from packages

--- resources/views/vendor/filament/components/layouts/app/sidebar/footer.blade.php
+++ resources/views/vendor/filament/components/layouts/app/sidebar/footer.blade.php
@@ -20,10 +20,18 @@
             </p>
         </a>
 
-        <p class="text-xs text-gray-500 hover:text-gray-700 focus:text-gray-700">
-            <a href="{{ route('filament.auth.logout') }}">
-                {{ __('filament::layout.buttons.logout.label') }}
-            </a>
-        </p>
+        <form action="{{ route('filament.auth.logout') }}" method="post" class="text-sm">
+            @csrf
+
+            <button
+                type="submit"
+                @class([
+                    'text-gray-600 hover:text-primary-500 focus:outline-none focus:underline',
+                    'dark:text-gray-300 dark:hover:text-primary-500' => config('filament.dark_mode'),
+                ])
+            >
+                {{ __('filament::widgets/account-widget.buttons.logout.label') }}
+            </button>
+        </form>
     </div>
 </footer>
