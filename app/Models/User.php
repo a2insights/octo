2b25a58cdf3067952bef5f1acdaa5eb31d1a536e
a21ns1g4ts <atila.danvi@outlook.com>
2023-04-15 01:08:52 -0300

    Fix user billing

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,6 @@ use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Cashier\Billable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements FilamentUser
@@ -15,7 +14,6 @@ class User extends Authenticatable implements FilamentUser
     use HasFactory;
 
     use Notifiable;
-    use Billable;
 
     /**
      * The attributes that are mass assignable.
