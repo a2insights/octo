b8fcfc956c3b2aa0b30e1c58ab3ca1a7ae479292
a21ns1g4ts <atila.danvi@outlook.com>
2023-05-06 20:11:05 -0300

    Add brezy plugin

--- config/filament.php
+++ config/filament.php
@@ -53,7 +53,7 @@ return [
     'auth' => [
         'guard' => env('FILAMENT_AUTH_GUARD', 'web'),
         'pages' => [
-            'login' => \Filament\Http\Livewire\Auth\Login::class,
+            'login' => \JeffGreco13\FilamentBreezy\Http\Livewire\Auth\Login::class,
         ],
     ],
 
