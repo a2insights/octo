c42c7836eaef6f20f4696cfc253bcd18333ef2a1
a21ns1g4ts <atila.danvi@outlook.com>
2023-05-29 15:05:14 -0300

    Composer update

--- database/migrations/2021_12_31_052353_create_settings_table.php
+++ database/migrations/2021_12_31_052353_create_settings_table.php
@@ -4,19 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSettingsTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
         Schema::create('settings', function (Blueprint $table): void {
             $table->id();
 
-            $table->string('group')->index();
+            $table->string('group');
             $table->string('name');
-            $table->boolean('locked');
+            $table->boolean('locked')->default(false);
             $table->json('payload');
 
             $table->timestamps();
+
+            $table->unique(['group', 'name']);
         });
     }
-}
+
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+};
