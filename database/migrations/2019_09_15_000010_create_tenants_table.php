b527922b2d451032090a4fe115bb6be63b7040df
a21ns1g4ts <atila.danvi@outlook.com>
2023-10-11 00:56:16 -0300

    Add tenancy defaults

---
+++ database/migrations/2019_09_15_000010_create_tenants_table.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTenantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tenants', function (Blueprint $table) {
+            $table->string('id')->primary();
+
+            // owner of the tenant
+            $table->string('owner_id')->index();
+
+            // your custom columns may go here
+
+            $table->timestamps();
+            $table->json('data')->nullable();
+
+            $table->foreign('owner_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tenants');
+    }
+}
