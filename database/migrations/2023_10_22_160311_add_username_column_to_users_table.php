e8921592e8e09bad029058108f6cb87fef3d8223
a21ns1g4ts <atila.danvi@outlook.com>
2023-11-16 01:54:40 -0300

    Upgrades from octo core

--- database/migrations/2023_10_22_160311_add_username_column_to_users_table.php
+++ database/migrations/2023_10_22_160311_add_username_column_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('username')->nullable();
+            $table->string('username')->nullable()->unique()->after('name');
         });
     }
 
