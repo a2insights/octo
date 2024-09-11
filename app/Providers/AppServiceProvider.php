b83b849081e219053e82d6c06901f57ec09a96ba
a21ns1g4ts <atila.danvi@outlook.com>
2024-09-10 22:25:58 -0300

    Fix service provider and wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,17 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Policies\ActivityPolicy;
+use BezhanSalleh\FilamentExceptions\Models\Exception;
+use Croustibat\FilamentJobsMonitor\Models\QueueMonitor;
+use HusamTariq\FilamentDatabaseSchedule\Models\Schedule;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Marjose123\FilamentWebhookServer\Models\FilamentWebhookServer;
+use SolutionForest\FilamentFirewall\Models\Ip;
+use Spatie\Activitylog\Models\Activity;
+use Spatie\Permission\Models\Role;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +29,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::policy(Ip::class, \App\Policies\IpPolicy::class);
+        Gate::policy(QueueMonitor::class, \App\Policies\QueueMonitorPolicy::class);
+        Gate::policy(FilamentWebhookServer::class, \App\Policies\WebhookPolicy::class);
+        Gate::policy(Role::class, \App\Policies\RolePolicy::class);
+        Gate::policy(User::class, \App\Policies\UserPolicy::class);
+        Gate::policy(Schedule::class, \App\Policies\SchedulePolicy::class);
+        Gate::policy(Activity::class, ActivityPolicy::class);
+        Gate::policy(Exception::class, \App\Policies\ExceptionPolicy::class);
     }
 }
