bd9475ae8b59067f4d7810438f56c61e4b58915b
a21ns1g4ts <atila.danvi@outlook.com>
2024-10-17 23:24:49 -0300

    Add subscription stubs

---
+++ app/Filament/Resources/SubscriptionResource/Pages/ListSubscriptions.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\SubscriptionResource\Pages;
+
+use App\Filament\Resources\SubscriptionResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListSubscriptions extends ListRecords
+{
+    protected static string $resource = SubscriptionResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
