8e8d88e0252e27ba375769eb3e588eba88ed4013
a21ns1g4ts <atila.danvi@outlook.com>
2024-10-31 02:54:10 -0300

    Wip packages

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,6 @@ use Filament\Panel;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
