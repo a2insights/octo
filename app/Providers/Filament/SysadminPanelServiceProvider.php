0fe4ba84f93251bab3c1504e87bd5a55c7042ab1
a21ns1g4ts <atila.danvi@outlook.com>
2024-11-28 23:52:22 -0300

    Lint provider

--- app/Providers/Filament/SysadminPanelServiceProvider.php
+++ app/Providers/Filament/SysadminPanelServiceProvider.php
@@ -75,7 +75,6 @@ class SysadminPanelServiceProvider extends PanelProvider
                 \SolutionForest\FilamentFirewall\FilamentFirewallPanel::make(),
                 \pxlrbt\FilamentEnvironmentIndicator\EnvironmentIndicatorPlugin::make(),
                 \BezhanSalleh\FilamentExceptions\FilamentExceptionsPlugin::make(),
-                \Firefly\FilamentBlog\Blog::make(),
                 // \BezhanSalleh\FilamentGoogleAnalytics\FilamentGoogleAnalyticsPlugin::make(),
                 \Croustibat\FilamentJobsMonitor\FilamentJobsMonitorPlugin::make()
                     ->label('Job')
