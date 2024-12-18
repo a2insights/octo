6cd8be926e8c8ae232654e72125509f00990f38b
a21ns1g4ts <atila.danvi@outlook.com>
2024-12-17 22:20:53 -0300

    Wip migrations

--- config/filament-saas.php
+++ config/filament-saas.php
@@ -1,14 +1,13 @@
 <?php
 
 return [
-    'sysadmin_path' => 'sysadmin',
-    'blog_path' => 'blog',
-    'tenant_path' => 'admin',
-    'site_path' => '/',
-
     'users' => [
-        'model' => App\Models\User::class,
-        'resource' => \A2Insights\FilamentSaas\User\Filament\UserResource::class,
+        'model' => A2Insights\FilamentSaas\User\User::class,
+        'resource' => A2Insights\FilamentSaas\User\Filament\UserResource::class,
         'tenant_scope' => false,
+    ],
+
+    'companies' => [
+        'model' => A2Insights\FilamentSaas\Tenant\Company::class,
     ],
 ];
